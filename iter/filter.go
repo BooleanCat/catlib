75687c5fe64b9b31349b201b3ee86f1d8a290238
Tom Godkin <tomgodkin@pm.me>
2022-01-22 15:22:12 +0000

    Implement exclude iter

--- iter/filter.go
+++ iter/filter.go
@@ -25,3 +25,9 @@ func (iter *FilterIter[T]) Next() catlib.Option[T] {
 }
 
 var _ Iterator[struct{}] = new(FilterIter[struct{}])
+
+func Exclude[T any](iter Iterator[T], f func(T) bool) *FilterIter[T] {
+	return &FilterIter[T]{iter, func(t T) bool {
+		return !f(t)
+	}}
+}
--- iter/filter_test.go
+++ iter/filter_test.go
@@ -20,3 +20,17 @@ func TestFilterZero(t *testing.T) {
 	filtered := iter.Filter[int](iter.Take[int](iter.Count(), 0), all)
 	assert.True(t, filtered.Next().IsNone())
 }
+
+func TestExclude(t *testing.T) {
+	isEven := func(i int) bool { return i%2 == 0 }
+	filtered := iter.Exclude[int](iter.Count(), isEven)
+	assert.Equal(t, filtered.Next().Unwrap(), 1)
+	assert.Equal(t, filtered.Next().Unwrap(), 3)
+	assert.Equal(t, filtered.Next().Unwrap(), 5)
+}
+
+func TestExcludeZero(t *testing.T) {
+	all := func(i int) bool { return true }
+	filtered := iter.Exclude[int](iter.Take[int](iter.Count(), 0), all)
+	assert.True(t, filtered.Next().IsNone())
+}
