a7ae97d7099e8801ca00bebb01924d482df32ae4
Tom Godkin <tomgodkin@pm.me>
2022-01-22 15:16:29 +0000

    Implement filter iter

---
+++ iter/filter.go
@@ -0,0 +1,27 @@
+package iter
+
+import "github.com/BooleanCat/catlib"
+
+type FilterIter[T any] struct {
+	iter Iterator[T]
+	f    func(T) bool
+}
+
+func Filter[T any](iter Iterator[T], f func(T) bool) *FilterIter[T] {
+	return &FilterIter[T]{iter, f}
+}
+
+func (iter *FilterIter[T]) Next() catlib.Option[T] {
+	for {
+		next := iter.iter.Next()
+		if next.IsNone() {
+			return next
+		}
+
+		if iter.f(next.Unwrap()) {
+			return next
+		}
+	}
+}
+
+var _ Iterator[struct{}] = new(FilterIter[struct{}])
---
+++ iter/filter_test.go
@@ -0,0 +1,22 @@
+package iter_test
+
+import (
+	"testing"
+
+	"github.com/BooleanCat/catlib/internal/assert"
+	"github.com/BooleanCat/catlib/iter"
+)
+
+func TestFilter(t *testing.T) {
+	isEven := func(i int) bool { return i%2 == 0 }
+	filtered := iter.Filter[int](iter.Count(), isEven)
+	assert.Equal(t, filtered.Next().Unwrap(), 0)
+	assert.Equal(t, filtered.Next().Unwrap(), 2)
+	assert.Equal(t, filtered.Next().Unwrap(), 4)
+}
+
+func TestFilterZero(t *testing.T) {
+	all := func(i int) bool { return true }
+	filtered := iter.Filter[int](iter.Take[int](iter.Count(), 0), all)
+	assert.True(t, filtered.Next().IsNone())
+}
